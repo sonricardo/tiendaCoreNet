3edb54c73e88bb3401f524f021cca7c45b6d4c33
sonricardo <sonricardo18@gmail.com>
2019-06-14 22:58:28 -0500

    Add files via upload

---
+++ Sistema.Entidades/almacen/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema.Entidades.almacen
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =3,ErrorMessage ="El nombre debe tener entre 3 y 50 caracteres")]
+        public string Nombre { get; set; }
+        [StringLength(256)]
+        public string descripcion { get; set; }
+        public bool Condicion { get; set; }
+
+    }
+}
